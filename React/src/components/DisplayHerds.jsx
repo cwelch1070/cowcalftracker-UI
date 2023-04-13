36a8da81436363c1bdde8b4d31d011fe6eaaf3c7
Caleb Welch <calebwelch233@gmail.com>
2023-04-12 22:43:15 -0500

    Added options drop down and additional styling

--- React/src/components/DisplayHerds.jsx
+++ React/src/components/DisplayHerds.jsx
@@ -1,5 +1,14 @@
 import react, { useEffect, useState } from 'react'
 import { getToken } from '../auth/store-token'
+import '../css/DisplayHerds.css'
+
+function Alert() {
+    return (
+        <div className="alert alert-success" role="alert">
+            Herd has been saved.
+        </div>
+    )
+}
 
 function CreateHerdButton() {
     return (
@@ -20,7 +29,7 @@ function CreateHerdButton() {
     )
 }
 
-function CreateHerdModal() {
+function CreateHerdModal(props) {
     const [herdName, setHerdName] = useState('')
     const createHerdRequest = async (herdName) => {
         const response = await fetch('http://45.58.52.73:81/herd', {
@@ -35,7 +44,7 @@ function CreateHerdModal() {
             }
         })
         const data = await response.json()
-        
+        props.getHerdData()
     }
 
     return (
@@ -80,9 +89,38 @@ function CreateHerdModal() {
     )
 }
 
-function Card() {
-    const [herds, setHerds] = useState([])
+function Card({herds}) {
+    return (
+        <>
+            {herds.length > 0 && (
+                <div>
+                    {herds.map(herd => (
+                        <div key={herd._id} className="card mb-2 mt-2">
+                            <div className="card-body" id='herd-card'>
+                                <div id='title-group'>
+                                    <h5 className="card-title mb-3" id='herd-name'>{herd.name}</h5>
+                                    <div className='dropdown'>
+                                        <button id='options-btn' className='btn dropdown-toggle' type='button' data-bs-toggle='dropdown'>. . .</button>
+                                        <ul className='dropdown-menu'>
+                                            <li className='dropdown-item'>Cattle</li>
+                                            <li className='dropdown-item'>Delete Herd</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <p className="card-text">Cattle: {herd.numOfCattle}</p>
+                                <p className='card-text'>Last Checked: {herd.dateCreated}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </>
+    )
+}
 
+export default function DisplayHerds() {
+
+    const [herds, setHerds] = useState([])
     const getHerdData = async () => {
         const response = await fetch('http://45.58.52.73:81/herd', {
             method: 'GET',
@@ -104,31 +142,10 @@ function Card() {
 
     return (
         <>
-            {herds.length > 0 && (
-                <div>
-                    {herds.map(herd => (
-                        <div key={herd._id} className="card mb-2 mt-2">
-                            <div className="card-body">
-                                <h5 className="card-title">{herd.name}</h5>
-                                <p className="card-text">Cattle: {herd.numOfCattle}</p>
-                                <p className='card-text'>Last Checked: {herd.dateCreated}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </>
-    )
-}
-
-export default function DisplayHerds() {
-
-    return (
-        <>
             <h1 className="display display-4">Available Herds</h1>
             <CreateHerdButton />
-            <Card />
-            <CreateHerdModal />
+            <Card herds={herds}  />
+            <CreateHerdModal getHerdData={getHerdData} />
         </>
     )
 }
