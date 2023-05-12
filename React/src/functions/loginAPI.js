054676bd9689c4af1ed6f545d705cae6abbd1d48
Caleb Welch <90748206+cwelch1070@users.noreply.github.com>
2023-05-12 00:10:32 -0500

    Sorting out issues with request to proxy

--- React/src/functions/loginAPI.js
+++ React/src/functions/loginAPI.js
@@ -6,7 +6,7 @@ const api = process.env.REACT_APP_API
 
 // Sends request to server to login user
 export const login = async (email, password) => {
-    const res = await fetch(`${api}/user/login`, {
+    const res = await fetch('https://www.cowcalftracker.com/api/user/login', {
         method: 'POST',
         mode: 'cors',
         body: JSON.stringify({
