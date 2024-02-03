0a648b5f974e366a094606358c86490b47e3a277
Caleb Welch <calebwelch233@gmail.com>
2024-02-03 13:09:23 -0600

    Added checkboxs for attendance page and navbar

--- React/src/components/Attendance.jsx
+++ React/src/components/Attendance.jsx
@@ -1,19 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom'
+import { getCows } from '../API-Requests/cattleAPI'
+import Navbar from './NavBar'
+import '../css/CattleCheckbox.css'
 
-function Checklist() {
-
+function Checklist({ cattle }) {
     return (
         <div>
-            Welcome to the attendance page!
+            {cattle.length > 0 && (
+                <div>
+                    {cattle.map((cow) => (
+                        <div key={cow._id} className="form-check" id="cattle-check-box">
+                             <input type="checkbox" className="btn-check" id={cow._id} unchecked autocomplete="off"></input>
+                             <label className="btn btn-outline-success" for={cow._id}>{cow.name} {cow.tag}</label>
+                             <br></br>
+                        </div>
+                    ))}
+                 </div>    
+                )}   
+                   
         </div>
     )
 }
 
 export default function Attendance() {
+    const location = useLocation()
+    const { herdId, herdName } = location.state
+    const [cattle, setCattle] = useState([])
+
+    const getCattleData = async () => {
+        const data = await getCows(herdId)
+        setCattle(data)
+    }
+
+    useEffect(() => {
+        getCattleData()
+    }, [])
 
     return (
         <>
-            <Checklist />
+            <div>
+                <Navbar />
+            </div>
+            <div className="container">
+                <h1 className="display display-4">{herdName}</h1>
+                <Checklist cattle = {cattle} />
+            </div>
         </>
     )
 }
